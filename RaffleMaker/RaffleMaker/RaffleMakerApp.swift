b476514cca28882fb7e395dcee852e8d3b9e5da5
Eric Davenport <Ericdavenport@pursuit.org>
2021-05-28 13:08:52 -0400

    Xcode project added to file

---
+++ RaffleMaker/RaffleMaker/RaffleMakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaffleMakerApp.swift
+//  RaffleMaker
+//
+//  Created by Eric Davenport on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct RaffleMakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
